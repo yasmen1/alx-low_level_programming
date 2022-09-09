d2b4d7c0dfc1cdeeadfff4f78332c59c06f98c6d
yasmen1 <yasooyzy@gmail.com>
2022-09-08 17:08:29 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * return: always o (success)
+ */
+
+int main (void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return 0;
+}
