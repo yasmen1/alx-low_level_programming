d4edbaec5089efa0a62cf0208ba9bac4ad8d914d
yasmen tarek <81187025+yasmen1@users.noreply.github.com>
2022-09-09 02:20:25 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,8 @@
  * this program prints "Programming is like building a multilingual puzzle
  * Return: 0
  */
-int main (void)
+int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle"");
 	return (0);
 }
