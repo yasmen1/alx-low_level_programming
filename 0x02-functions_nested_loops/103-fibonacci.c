b5163edc6a59e84acd2e9a46dde2097463273f21
yasmen tarek <81187025+yasmen1@users.noreply.github.com>
2022-09-14 00:40:49 +0200

    15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	return (0);
+}
+Footer
+© 2022 GitHub, Inc.
