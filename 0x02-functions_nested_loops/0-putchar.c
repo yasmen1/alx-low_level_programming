97944764a22c22fd6dbd0594dc2b36411ac37291
yasmen tarek <81187025+yasmen1@users.noreply.github.com>
2022-09-14 00:10:43 +0200

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main- entr point
+ * main - entry point
  *
  * return: 0
  */
@@ -9,7 +9,7 @@ int main(void)
 {
 char c[] = "_putchar";
 int i;
-for (i = 0; i < 9; i++)
+for (i = 0; i < 7; i++)
 {
 _putchar(c[i]);
 }
