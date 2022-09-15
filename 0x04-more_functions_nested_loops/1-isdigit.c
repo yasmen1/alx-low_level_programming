0acfd2531c906d20869ecb38949b20016c420fde
yasmen tarek <81187025+yasmen1@users.noreply.github.com>
2022-09-15 20:55:51 +0200

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,8 +6,12 @@
 *Return: 1 if it is, 0 otherwise
 */
 
-int _isdigit(int c){
-    if(c>=48 && c<=57){
-      return (1);}
-return (0);
+int _isdigit(int c)
+{
+	if ((c >= 48) && (c <= 57))
+	{
+		return (1);
+	}
+
+	return (0);
 }
