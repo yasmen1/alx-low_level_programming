880880caaf7060702b3e10f336ac43f8a428773e
yasmen tarek <81187025+yasmen1@users.noreply.github.com>
2022-09-15 18:00:00 +0200

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,9 +3,9 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int );
-int largest_number(int, int, int);
-void print_remaining_days(int, int, int);
-int convert_day(int, int);
+void positive_or_negative(int n);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
