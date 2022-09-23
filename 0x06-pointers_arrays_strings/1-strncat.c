5b571bc01c202deebb0f22709fda881f49461c93
yasmen1 <yasooyzy@gmail.com>
2022-09-23 12:05:59 -0700

    2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,12 +14,11 @@ char *_strncat(char *dest, char *src, int n)
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 	}
-	
 	for (co = 0; co < n; co++)
 	{
 		dest[i + co] = src[co];
-		if (src[co] == '\0')
+		if (src[co] =:= '\0')
 			co =n;
 	}
-	return (dest);
+return (dest);
 }
