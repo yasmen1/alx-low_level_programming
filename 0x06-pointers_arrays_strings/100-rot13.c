58b61c092f24c144f32a43e40b9810f572505571
yasmen tarek <81187025+yasmen1@users.noreply.github.com>
2022-09-23 21:25:15 +0200

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,22 +1,29 @@
 #include "main.h"
-/**
- * print_number - prints an integer
- * @n:.input integer parameter
- *
- *
- */
-void print_number(int n)
-{
-	unsigned int i = n;
 
-	if (n < 0)
+/**
+ * *rot13 - encodes a string using rot13.
+ * @s: int type array pointer
+ * Return: encoded
+ */
+
+char *rot13(char *s)
+{
+	int i, ii;
+	char input[] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		_putchar(45);
-		i = -i;
+		for (ii = 0; ii < 54; ii++)
+		{
+			if (((s[i] <= 'z' && s[i] >= 'a') || (s[i] <= 'Z' && s[i] >= 'A'))
+		&& s[i] == input[ii])
+			{
+				s[i] = output[ii];
+				break;
+			}
+		}
 	}
-	if (i / 10)
-	{
-		print_number(i / 10);
-	}
-	_putchar(i % 10 + '0');
+
+	return (s);
 }
