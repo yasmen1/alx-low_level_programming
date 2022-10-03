aff42296a2dcd529ec8a927400e35c0ccd010b45
yasmen tarek <81187025+yasmen1@users.noreply.github.com>
2022-10-03 12:18:13 +0200

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,1 +1,38 @@
-#include "main.h"                                                                     #include<stdlib.h>                                                                    /**                                                                                    * _strdup - returns a pointer to a newely allocated space in memory,                  * which contains a copy of the string given as a parameter.                           *                                                                                     * @str: string to copy                                                                *                                                                                     * Return: pointer to array, or NULL.                                                  */                                                                                   char *_strdup(char *str)                                                              {                                                                                             char *r;                                                                              int i, j;                                                                             int coun = 0;                                                                                                                                                               if (str == NULL)                                                                      {                                                                                             return (NULL);                                                                }                                                                                     for (i = 0; str[i] != '\0'; i++)                                                      {                                                                                             coun = coun + 1;                                                              }                                                                                                                                                                           r = malloc(sizeof(char) * coun + 1);                                                                                                                                        if (r == NULL)                                                                        {                                                                                             return (NULL);                                                                }                                                                                     for (j = 0; str[j] != '\0'; j++)                                                      {                                                                                             r[j] = str[j];                                                                }                                                                                                                                                                           r[j] = '\0';                                                                                                                                                                return (r);                                                                   } 
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * *_strdup - returns a pointer to a newly allocated space with copy of arg
+ * @str: string to copy
+ * Return: pointer to array or null
+ **/
+char *_strdup(char *str)
+{
+	char *r;
+	int i, j;
+	int coun = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		coun = coun + 1;
+	}
+
+	r = malloc(sizeof(char) * coun + 1);
+
+	if (r == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; str[j] != '\0' ; j++)
+	{
+		r[j] = str[j];
+	}
+
+	r[j] = '\0';
+
+	return (r);
+}
