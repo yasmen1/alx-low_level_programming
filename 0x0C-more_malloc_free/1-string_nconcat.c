3ad786134a628266fe64fbeccd9edcf7b0d44c5e
yasmen tarek <81187025+yasmen1@users.noreply.github.com>
2022-11-23 19:27:42 +0200

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,16 +1,14 @@
 #include "main.h"
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
 /**
- * char *string_nconcat -  function that concatenates two strings
- *
- * @s1: pntr to char
- * @s2: pntr to char
- * @n: unsigned int
- * Return: pointer to newly allocated space in memory, NULL.
- */
-
+  * string_nconcat - ...
+  * @s1: ...
+  * @s2: ...
+  * @n: ...
+  *
+  * Return: ...
+  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int i = 0, j = 0, k = 0, l = 0;
@@ -20,6 +18,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+
 	while (s1[i])
 		i++;
 
@@ -34,18 +33,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	str = malloc(sizeof(char) * l + 1);
 	if (str == NULL)
 		return (NULL);
+
 	k = 0;
-	while (j < 1)
+	while (j < l)
 	{
 		if (j <= i)
+			str[j] = s1[j];
+
+		if (j >= i)
 		{
 			str[j] = s2[k];
 			k++;
 		}
 		j++;
-
 	}
 	str[j] = '\0';
 	return (str);
-
 }
