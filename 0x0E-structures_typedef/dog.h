7f0009524ed606e4e425dbd9f10ed56bdbace92e
yasmen1 <yasooyzy@gmail.com>
2022-11-23 10:48:12 -0800

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+typedef struct dog dog_t;
+/*
+ * struct dog - create a dog structure
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
